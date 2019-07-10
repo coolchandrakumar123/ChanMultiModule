117a7d0b70efef77541c51951b544ac0e4793156
chandra-1765 <chandrakumar.selvaraj@zohocorp.com>
2019-07-10 15:45:16 +0530

    modules added

---
+++ androidXModule/src/main/java/com/zoho/desk/chanmultimodule/AndroidXModuleActivity.kt
@@ -0,0 +1,12 @@
+package com.zoho.desk.chanmultimodule
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class AndroidXModuleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.androidx_module_layout)
+    }
+}
---
+++ supportModule/src/main/java/com/zoho/desk/chanmultimodule/SupportModuleActivity.kt
@@ -0,0 +1,12 @@
+package com.zoho.desk.chanmultimodule
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class SupportModuleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.support_module_layout)
+    }
+}
